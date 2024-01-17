b42480dde4e92ae4466416a27dfa5cb306faa090
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-17 14:53:26 +0200

    create base entity

---
+++ Proiect final/Models/Base/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proiect_final.Models.Base
+{
+    public class BaseEntity : IBaseEntity
+    {
+        [Key]
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Required]
+
+        public Guid Id { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ Proiect final/Models/Base/IBaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace Proiect_final.Models.Base
+{
+    public interface IBaseEntity
+    {
+        Guid Id { get; set; }
+
+        DateTime CreatedAt { get; set; }
+
+        DateTime UpdatedAt { get; set; }
+    }
+}
