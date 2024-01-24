586bc4cb5d7d8643ac6602163e8f24d986cf58d7
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-24 04:07:17 +0200

    helpers mapper profile

--- Proiect final/Helpers/Extensions/ServiceExtensions.cs
+++ Proiect final/Helpers/Extensions/ServiceExtensions.cs
@@ -1,6 +1,22 @@
-﻿namespace Proiect_final.Helpers.Extensions
+﻿using Proiect_final.Repositories.BusRepository;
+using Proiect_final.Services.BusService;
+
+namespace Proiect_final.Helpers.Extensions
 {
-    public class ServiceExtensions
+    public static class ServiceExtensions
     {
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            //services.AddTransient<IRepository, Repository>();
+            services.AddTransient<IBusRepository, BusRepository>();
+            return services;
+        }
+
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            //services.AddTransient<IService,Service>();
+            services.AddTransient<IBusService, BusService>();
+            return services;
+        }
     }
 }
---
+++ Proiect final/Helpers/MapperProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Proiect_final.Models.Bus;
+using Proiect_final.Models.Bus.DTO;
+
+namespace Proiect_final.Helpers
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<Bus, BusResponseDto>();
+            CreateMap<BusRequestDto, Bus>();
+        }
+
+    }
+}
