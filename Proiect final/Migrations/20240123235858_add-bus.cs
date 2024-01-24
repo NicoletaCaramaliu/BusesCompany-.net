599735736a75c924427c8bb763918db4844ba40c
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-24 02:05:32 +0200

    add bus migration

---
+++ Proiect final/Migrations/20240123235858_add-bus.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Proiect_final.Migrations
+{
+    /// <inheritdoc />
+    public partial class addbus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Bus",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Number = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Capacity = table.Column<int>(type: "int", nullable: false),
+                    Route = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bus", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bus_Number",
+                table: "Bus",
+                column: "Number",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bus");
+        }
+    }
+}
