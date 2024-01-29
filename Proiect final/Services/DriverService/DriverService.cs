78b5a0c14b14ef199624a6875a0ab80cf55ecc87
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-29 14:08:21 +0200

    Driver service

---
+++ Proiect final/Services/DriverService/DriverService.cs
@@ -0,0 +1,28 @@
+﻿using Proiect_final.Models.Driver;
+using Proiect_final.Repositories.DriverRepository;
+
+namespace Proiect_final.Services.DriverService
+{
+    public class DriverService : IDriverService
+    {
+        private readonly IDriverRepository _driverRepository;
+
+        public DriverService(IDriverRepository driverRepository)
+        {
+            _driverRepository = driverRepository;
+        }
+
+        //get all drivers
+        public async Task<IEnumerable<Driver>> GetAllDrivers()
+        {
+            return await _driverRepository.GetAllAsync();
+        }
+
+        //create driver
+        public async Task CreateDriver(Driver driver)
+        {
+            await _driverRepository.CreateAsync(driver);
+            await _driverRepository.SaveAsync();
+        }
+    }
+}
---
+++ Proiect final/Services/DriverService/IDriverService.cs
@@ -0,0 +1,11 @@
+﻿using Proiect_final.Models.Driver;
+
+namespace Proiect_final.Services.DriverService
+{
+    public interface IDriverService
+    {
+        Task<IEnumerable<Driver>> GetAllDrivers();
+
+        Task CreateDriver(Driver driver);
+    }
+}
