1419f84254220750c9cbcab3bd681a7927e85959
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-30 00:18:55 +0200

    add model repir history many to many bus-defection

---
+++ Proiect final/Models/RepairHistory/RepairHistory.cs
@@ -0,0 +1,13 @@
+﻿using Proiect_final.Models.Base;
+
+namespace Proiect_final.Models.RepairHistory
+{
+    public class RepairHistory : BaseEntity
+    {
+        public Guid BusId { get; set; }
+        public Guid DefectionId { get; set;}
+
+        public Bus.Bus Bus { get; set; }
+        public Defection.Defection Defection { get; set; }
+    }
+}
