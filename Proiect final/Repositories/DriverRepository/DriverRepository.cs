3deec9b902634c479d4c71e68dffaf27a4f57aa9
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-29 14:09:15 +0200

    driver repository

---
+++ Proiect final/Repositories/DriverRepository/DriverRepository.cs
@@ -0,0 +1,15 @@
+﻿using Proiect_final.Data;
+using Proiect_final.Models.Driver;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.DriverRepository
+{
+    public class DriverRepository : GenericRepository<Driver>, IDriverRepository
+    {
+        public DriverRepository(ApiDbContext context) : base(context)
+        {
+
+        }
+
+    }
+}
---
+++ Proiect final/Repositories/DriverRepository/IDriverRepository.cs
@@ -0,0 +1,9 @@
+﻿using Proiect_final.Models.Driver;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.DriverRepository
+{
+    public interface IDriverRepository : IGenericRepository<Driver>
+    {
+    }
+}
