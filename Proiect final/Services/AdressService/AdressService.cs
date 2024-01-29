ef542cba10abaf8bfa79ed16ef14f454ae522233
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-30 00:15:19 +0200

    adress service

---
+++ Proiect final/Services/AdressService/AdressService.cs
@@ -0,0 +1,38 @@
+﻿using Proiect_final.Models.Adress;
+using Proiect_final.Models.Adress.DTO;
+using Proiect_final.Repositories.AdressRepository;
+using Proiect_final.Services.DriverService;
+
+namespace Proiect_final.Services.AdressService
+{
+    public class AdressService : IAdressService
+    {
+        private readonly IAdressRepository _adressRepository;
+        private readonly IDriverService _driverService;
+
+        public AdressService(IAdressRepository adressRepository, IDriverService driverService)
+        {
+            _adressRepository = adressRepository;
+            _driverService = driverService;
+        }
+
+        //get all adresses
+        public async Task<IEnumerable<Adress>> GetAllAdresses()
+        {
+            return await _adressRepository.GetAllAsync();
+        }
+
+        //create adress
+        public async Task CreateAdress(Adress adress)
+        {
+            await _adressRepository.CreateAsync(adress);
+            await _adressRepository.SaveAsync();
+        }
+
+        //get adress by driver id
+        public async Task<Adress> GetAdressByDriverId(Guid id)
+        {
+            return await _adressRepository.GetAdressByDriverId(id);
+        }
+    }
+}
---
+++ Proiect final/Services/AdressService/IAdressService.cs
@@ -0,0 +1,16 @@
+﻿using Proiect_final.Models.Adress;
+
+namespace Proiect_final.Services.AdressService
+{
+    public interface IAdressService
+    {
+        //get all adresses
+        Task<IEnumerable<Adress>> GetAllAdresses();
+
+        //create adress
+        Task CreateAdress(Adress adress);
+
+        //get adress by driver id
+        Task<Adress> GetAdressByDriverId(Guid driverId);
+    }
+}
