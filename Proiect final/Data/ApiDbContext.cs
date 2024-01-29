e920ad4c681c59fc131f49c8f8238c56a2f95c14
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-30 00:21:08 +0200

    add adress defections repair history, many to many relationship

--- Proiect final/Data/ApiDbContext.cs
+++ Proiect final/Data/ApiDbContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Proiect_final.Models.Adress;
 using Proiect_final.Models.Bus;
+using Proiect_final.Models.Defection;
 using Proiect_final.Models.Driver;
+using Proiect_final.Models.RepairHistory;
 
 
 namespace Proiect_final.Data
@@ -9,6 +12,11 @@ namespace Proiect_final.Data
     {
         public DbSet<Bus> Buses { get; set; }
         public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Adress> Adresses { get; set; }
+        public DbSet<Defection> Defections { get; set; }
+        public DbSet<RepairHistory> RepairHistories { get; set; }
+
+
 
         public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
         {
@@ -27,6 +35,25 @@ namespace Proiect_final.Data
                 .HasOne(d => d.Bus)
                 .WithMany(b => b.Drivers)
                 .HasForeignKey(d => d.BusId);
+
+            //one to one relationship between driver and adress
+            modelBuilder.Entity<Driver>()
+                .HasOne(d => d.Adress)
+                .WithOne(d => d.Driver)
+                .HasForeignKey<Adress>(a => a.DriverId);
+
+            //many to many relationship between bus and defection, association table is RepairHistory
+            modelBuilder.Entity<RepairHistory>()
+                .HasKey(r => new {r.BusId, r.DefectionId});
+            modelBuilder.Entity<RepairHistory>()
+                .HasOne(r => r.Bus)
+                .WithMany(b => b.RepairHistories)
+                .HasForeignKey(r => r.BusId);
+            modelBuilder.Entity<RepairHistory>()
+                .HasOne(r => r.Defection)
+                .WithMany(d => d.RepairHistories)
+                .HasForeignKey(r => r.DefectionId);
+
 
             base.OnModelCreating(modelBuilder);
         }
