e4575a55007f9899fbe8d0bdaf20d0caaa8b88b4
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-30 00:16:32 +0200

    adress repository-get adress by driver id

---
+++ Proiect final/Repositories/AdressRepository/AdressRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proiect_final.Data;
+using Proiect_final.Models.Adress;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.AdressRepository
+{
+    public class AdressRepository : GenericRepository<Adress>, IAdressRepository
+    {
+        public AdressRepository(ApiDbContext context) : base(context)
+        {
+        }
+        //get adress by driver id
+        public async Task<Adress> GetAdressByDriverId(Guid driverId)
+        {
+            return await _table
+                .FirstOrDefaultAsync(a => a.DriverId == driverId);
+        }
+    }
+}
---
+++ Proiect final/Repositories/AdressRepository/IAdressRepository.cs
@@ -0,0 +1,12 @@
+﻿using Proiect_final.Models.Adress;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.AdressRepository
+{
+    public interface IAdressRepository : IGenericRepository<Adress>
+    {
+        //get adress by driver id
+        Task<Adress> GetAdressByDriverId(Guid driverId);
+    }
+
+}
