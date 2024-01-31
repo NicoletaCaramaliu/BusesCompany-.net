6bb7f6df6dd348838716fe58791977367458aad5
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-01-31 02:06:23 +0200

    change repairHistory attributes

--- Proiect final/Models/RepairHistory/RepairHistory.cs
+++ Proiect final/Models/RepairHistory/RepairHistory.cs
@@ -6,6 +6,8 @@ namespace Proiect_final.Models.RepairHistory
     {
         public Guid BusId { get; set; }
         public Guid DefectionId { get; set;}
+        public DateTime RepairDate { get; set; }
+        public int Price { get; set; }
 
         public Bus.Bus Bus { get; set; }
         public Defection.Defection Defection { get; set; }
