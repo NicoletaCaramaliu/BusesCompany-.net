4150a7c91692bd94d9f3580c64d847911b413d0f
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-02-03 18:45:47 +0200

    defection dto

---
+++ Proiect final/Models/Defection/DTO/DefectionRequestDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Proiect_final.Models.Defection.DTO
+{
+    public class DefectionRequestDto
+    {
+        public string DefectionName { get; set; }
+    }
+}
---
+++ Proiect final/Models/Defection/DTO/DefectionResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Proiect_final.Models.Defection.DTO
+{
+    public class DefectionResponseDto
+    {
+        public Guid Id { get; set; }
+        public string DefectionName { get; set; }
+    }
+}
