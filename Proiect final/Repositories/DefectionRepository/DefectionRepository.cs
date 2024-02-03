12d295f9b582afc54aa3002f3cad703a6c8cf0b0
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-02-03 18:47:39 +0200

    create defection repository

---
+++ Proiect final/Repositories/DefectionRepository/DefectionRepository.cs
@@ -0,0 +1,15 @@
+﻿using Proiect_final.Data;
+using Proiect_final.Models.Defection;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.DefectionRepository
+{
+    public class DefectionRepository : GenericRepository<Defection>, IDefectionRepository
+    {
+        public DefectionRepository(ApiDbContext context) : base(context)
+        {
+        }
+
+        
+    }
+}
---
+++ Proiect final/Repositories/DefectionRepository/IDefectionRepository.cs
@@ -0,0 +1,9 @@
+﻿using Proiect_final.Models.Defection;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.DefectionRepository
+{
+    public interface IDefectionRepository : IGenericRepository<Defection>
+    {
+    }
+}
