659b1e25b1a908b880486af84a382ec3b978b33d
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-02-04 00:46:52 +0200

    Repair history controller- update delete get by id

--- Proiect final/Controllers/RepairHistoryController.cs
+++ Proiect final/Controllers/RepairHistoryController.cs
@@ -36,5 +36,42 @@ namespace Proiect_final.Controllers
             var repairHistoryResponseDto = _mapper.Map<RepairHistoryResponseDto>(repairHistory);
             return Ok(repairHistoryResponseDto);
         }
+
+        //get repair history by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<RepairHistoryResponseDto>> GetRepairHistoryById(Guid id)
+        {
+            var repairHistory = await _repairHistoryService.GetRepairHistoryById(id);
+            var repairHistoryResponseDto = _mapper.Map<RepairHistoryResponseDto>(repairHistory);
+            return Ok(repairHistoryResponseDto);
+        }
+
+        //update repair history
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<RepairHistoryResponseDto>> UpdateRepairHistory(Guid id, RepairHistoryRequestDto repairHistoryRequestDto)
+        {
+            var repairHistory = await _repairHistoryService.GetRepairHistoryById(id);
+            if (repairHistory == null)
+            {
+                return NotFound($"Repair history with ID {id} not found");
+            }
+            _mapper.Map(repairHistoryRequestDto, repairHistory);
+            await _repairHistoryService.UpdateRepairHistory(repairHistory);
+            var repairHistoryResponseDto = _mapper.Map<RepairHistoryResponseDto>(repairHistory);
+            return Ok(repairHistoryResponseDto);
+        }
+
+        //delete repair history
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteRepairHistory(Guid id)
+        {
+            var deletedRepairHistory = await _repairHistoryService.GetRepairHistoryById(id);
+            if (deletedRepairHistory == null)
+            {
+                return NotFound($"Repair history with ID {id} not found");
+            }
+            await _repairHistoryService.DeleteRepairHistory(deletedRepairHistory);
+            return NoContent();
+        }
     }
 }
