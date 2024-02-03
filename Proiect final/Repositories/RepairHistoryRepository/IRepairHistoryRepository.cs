b65b006937796819731d0144e2fcb3914d8b2564
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-02-03 18:47:59 +0200

    create repair history repository

---
+++ Proiect final/Repositories/RepairHistoryRepository/IRepairHistoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Proiect_final.Models.RepairHistory;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.RepairHistoryRepository
+{
+    public interface IRepairHistoryRepository : IGenericRepository<RepairHistory>
+    {
+    }
+}
---
+++ Proiect final/Repositories/RepairHistoryRepository/RepairHistoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using Proiect_final.Data;
+using Proiect_final.Models.RepairHistory;
+using Proiect_final.Repositories.GenericRepository;
+
+namespace Proiect_final.Repositories.RepairHistoryRepository
+{
+    public class RepairHistoryRepository : GenericRepository<RepairHistory>, IRepairHistoryRepository
+    {
+        public RepairHistoryRepository(ApiDbContext context) : base(context)
+        {
+        }
+    }
+}
