b2bcb475f41e64b9d01c47832525aee6bc580ae5
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-02-04 00:49:38 +0200

    add model user

---
+++ Proiect final/Models/Users/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Proiect_final.Models.Users
+{
+    public class UserDto
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
