2cc74cc98ed886e9e527ba6921e0b7939dce3634
NicoletaCaramaliu <115987932+NicoletaCaramaliu@users.noreply.github.com>
2024-02-03 18:44:23 +0200

    defection, repair history repositories

---
+++ Proiect final/Controllers/DefectionController.cs
@@ -0,0 +1,76 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Proiect_final.Models.Defection;
+using Proiect_final.Models.Defection.DTO;
+using Proiect_final.Services.DefectionService;
+
+namespace Proiect_final.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DefectionController : ControllerBase
+    {
+        private readonly IDefectionService _defectionService;
+        private readonly IMapper _mapper;
+        public DefectionController(IDefectionService defectionService, IMapper mapper)
+        {
+            _defectionService = defectionService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllDefections()
+        {
+            var defections = await _defectionService.GetAllDefections();
+            var defectionsDto = _mapper.Map<IEnumerable<DefectionResponseDto>>(defections);
+            return Ok(defectionsDto);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<DefectionResponseDto>> CreateDefection(DefectionRequestDto defectionRequestDto)
+        {
+            var defection = _mapper.Map<Defection>(defectionRequestDto);
+            await _defectionService.CreateDefection(defection);
+            var defectionResponseDto = _mapper.Map<DefectionResponseDto>(defection);
+            return Ok(defectionResponseDto);
+        }
+
+        //get defection by id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DefectionResponseDto>> GetDefectionById(Guid id)
+        {
+            var defection = await _defectionService.GetDefectionById(id);
+            var defectionResponseDto = _mapper.Map<DefectionResponseDto>(defection);
+            return Ok(defectionResponseDto);
+        }
+
+        //update defection
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<DefectionResponseDto>> UpdateDefection(Guid id, DefectionRequestDto defectionRequestDto)
+        {
+            var defection = await _defectionService.GetDefectionById(id);
+            if (defection == null)
+            {
+                return NotFound($"Defection with ID {id} not found");
+            }
+            _mapper.Map(defectionRequestDto, defection);
+            await _defectionService.UpdateDefection(defection);
+            var defectionResponseDto = _mapper.Map<DefectionResponseDto>(defection);
+            return Ok(defectionResponseDto);
+        }
+
+        //delete defection
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult> DeleteDefection(Guid id)
+        {
+            var defection = await _defectionService.GetDefectionById(id);
+            if (defection == null)
+            {
+                return NotFound($"Defection with ID {id} not found");
+            }
+            await _defectionService.DeleteDefection(defection);
+            return Ok();
+        }
+        
+    }
+}
---
+++ Proiect final/Controllers/RepairHistoryController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Proiect_final.Models.RepairHistory;
+using Proiect_final.Models.RepairHistory.DTO;
+using Proiect_final.Services.RepairHistoryService;
+
+namespace Proiect_final.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RepairHistoryController : ControllerBase
+    {
+        private readonly IRepairHistoryService _repairHistoryService;
+        private readonly IMapper _mapper;
+        public RepairHistoryController(IRepairHistoryService repairHistoryService, IMapper mapper)
+        {
+            _repairHistoryService = repairHistoryService;
+            _mapper = mapper;
+        }
+
+        //get all repair histories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RepairHistoryResponseDto>>> GetAllRepairHistories()
+        {
+            var repairHistories = await _repairHistoryService.GetAllRepairHistories();
+            var repairHistoriesDto = _mapper.Map<IEnumerable<RepairHistoryResponseDto>>(repairHistories);
+            return Ok(repairHistoriesDto);
+        }
+
+        //create repair history
+        [HttpPost]
+        public async Task<ActionResult<RepairHistoryResponseDto>> CreateRepairHistory(RepairHistoryRequestDto repairHistoryRequestDto)
+        {
+            var repairHistory = _mapper.Map<RepairHistory>(repairHistoryRequestDto);
+            await _repairHistoryService.CreateRepairHistory(repairHistory);
+            var repairHistoryResponseDto = _mapper.Map<RepairHistoryResponseDto>(repairHistory);
+            return Ok(repairHistoryResponseDto);
+        }
+    }
+}
